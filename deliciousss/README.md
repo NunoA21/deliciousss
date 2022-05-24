11f24c2d4626e3406a4378456b1ba97d31d18be9
Nuno Araújo <nuno.araujo@itsector.pt>
2022-05-24 16:41:13 +0100

    added packages + popular carousel implementation

--- deliciousss/README.md
+++ deliciousss/README.md
@@ -1,4 +1,6 @@
-# Getting Started with Create React App
+# Deliciousss mini project
+
+This project is for training purposes and it follows this video here [DevEd Tutorial](https://www.youtube.com/watch?v=xc4uOzlndAk&ab_channel=DevEd).
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
@@ -14,57 +16,19 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
-### `npm test`
+### `npm packages installed`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Here we'll have some packages installed that are going to be useful for this mini project:
 
-### `npm run build`
+1. **framer-motion:**
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+   This package is for some animations: `npm install framer-motion`.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+   In our project, we ran this command: `npm install framer-motion react-icons react-router-dom styled-components`
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+   This will instal some things for react routing, icons and some styled components that we will use in the project.
 
-### `npm run eject`
+2. **react-splide:**
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+   It's for the carousel animation.
+   This is the command we'll use: `npm install @splidejs/react-splide` (the command in the video didn't worked, so we had to use this one for it to work).
--- deliciousss/src/components/Popular.jsx
+++ deliciousss/src/components/Popular.jsx
@@ -1,4 +1,7 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Popular() {
   //no use state podemos inicializar com o que quisermos (array, string, bool, etc), apenas inicializamos com o tipo de dados que precisamos
@@ -11,25 +14,102 @@ function Popular() {
   }, []);
 
   const getPopular = async () => {
-    //em baixo vamos buscar 9 receitas ao calhas
-    //*NOTA: quando se adiciona um ficheiro .env, tem de se dar restart ao servidor para podermos aplicar as definições (Ctrl + C e depois npm start)
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
+    //primeiro verificamos se temos os dados em sessão, para não estarmosa gastar pedidos da API e para melhorarmos a performance da app
+    const check = localStorage.getItem("popular");
 
-    const data = await api.json();
-    //usamos o setPopular, que é uma função, a qual vamos passar o estado. Em React não se altera diretamente o estado
-    setPopular(data.recipes); //a api retorna um objecto onde tem uma lista com o nome recipes
+    if (check) {
+      //no localStorage só podemos guardar strings, por isso é qe aqui fazemos um parse para JSON e em baixo fazemos um stringify
+      setPopular(JSON.parse(check));
+    } else {
+      //em baixo vamos buscar 9 receitas ao calhas
+      //*NOTA: quando se adiciona um ficheiro .env, tem de se dar restart ao servidor para podermos aplicar as definições (Ctrl + C e depois npm start)
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+
+      //usamos o setPopular, que é uma função, a qual vamos passar o estado. Em React não se altera diretamente o estado
+      setPopular(data.recipes); //a api retorna um objecto onde tem uma lista com o nome recipes
+      console.log(data.recipes);
+    }
   };
 
   return (
-    <div>
-      {popular.map((recipe) => {
-        //tem d ese adicionar uma key para que se possa fazer o tracking das alterações
-        return <div key={recipe.id}>{recipe.title}</div>;
-      })}
-    </div>
+    <Wrapper>
+      <h3>Popoular Picks</h3>
+      <Splide
+        options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: "free",
+          gap: "5rem",
+        }}
+      >
+        {popular.map((recipe) => {
+          //tem d ese adicionar uma key para que se possa fazer o tracking das alterações
+          return (
+            <SplideSlide key={recipe.id}>
+              <Card>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title}></img>
+                <Gradient />
+              </Card>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
+    </Wrapper>
   );
 }
+
+//o styled components permite-nos criar este tipo de componentes
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+//aqui são as cartas onde vão estar as imagens
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+//O Gradient é o sombreado que fica nas cartas do carrossel para que fique mais perceptivel o nome dos pratos
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
 
 export default Popular;
